6c1ed58f11a75e5483ade48bd00cd36528834f8e
Evader <evaderfati@gmail.com>
2021-05-11 19:56:20 +0800

    Initial Commit

---
+++ Orbital/OrbitalApp.swift
@@ -0,0 +1,17 @@
+//
+//  OrbitalApp.swift
+//  Orbital
+//
+//  Created by Evader on 11/5/21.
+//
+
+import SwiftUI
+
+@main
+struct OrbitalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
